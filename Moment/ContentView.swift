593e7581810b4bf12cb14971a18a56fb06d697d0
kim <xiao.kim@tradego8.com>
2026-04-22 16:17:56 +0800

    Initial Commit

---
+++ Moment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moment
+//
+//  Created by kim on 2026/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moment/MomentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentApp.swift
+//  Moment
+//
+//  Created by kim on 2026/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MomentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
